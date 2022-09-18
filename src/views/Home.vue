3d93aa5a01a6f5f38d4c85143207eba2da0546e4
alamgirweb11 <alamgirweb11@gmail.com>
2022-09-18 17:45:08 +0600

    landing and postDetails page design done

---
+++ src/views/Home.vue
@@ -0,0 +1,221 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="mt-3 mb-2">
+    <b-row class="mb-2">
+      <b-form-group id="input-group-3" label-for="input-3">
+        <b-form-select
+          label="Filter:"
+          class="form-control"
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+    </b-row>
+    <b-row>
+      <b-col cols="9">
+        <b-row>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+          <b-col>
+            <b-card
+              title="Card Title"
+              img-src="https://via.placeholder.com/350x150"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+
+              <b-button  variant="primary"><router-link class="text-light" :to="{name: 'postDetails', params: {id:2}}">Read More...</router-link></b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="3">
+        <RightSidebar />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { BRow, BCol, BFormGroup, BFormSelect } from 'bootstrap-vue'
+import RightSidebar from '@/components/RightSidebar.vue'
+export default {
+  data: function () {
+    return {
+      form: {
+        food: null
+      },
+      foods: [
+        { text: 'Filter Post', value: null },
+        'Category-1',
+        'Category-2',
+        'Category-3',
+        'Category-4'
+      ],
+      show: true
+    }
+  },
+  components: {
+    RightSidebar,
+    BRow,
+    BCol,
+    BFormGroup,
+    BFormSelect
+  }
+}
+</script>
---
+++ src/views/PostDetails.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div class="mt-3 mb-2">
+      <b-row>
+        <b-col cols="9">
+        <h5>Title Tag</h5>
+        <img src="https://via.placeholder.com/350x150" alt="" srcset="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur soluta excepturi dignissimos provident illo tempore amet perspiciatis, facilis ea odio! Placeat, reiciendis eaque! Voluptatibus, modi. Ipsa quis iste aut delectus reiciendis. Praesentium corrupti ullam, molestiae rerum voluptates porro qui accusantium quas explicabo laboriosam accusamus autem. Reiciendis voluptate obcaecati, iste officiis laborum dicta magni facilis asperiores vero repellendus perferendis perspiciatis, aliquid rem dolorem! Dolorum accusamus ullam fugiat aperiam saepe inventore tempore rerum, aliquam odit iure? Fugit impedit facilis esse nulla nostrum quasi, recusandae ex perspiciatis nobis repellat reiciendis illum, nisi amet. Enim reiciendis rem earum ipsa vel facilis cumque, cum nam.</p>
+        </b-col>
+        <b-col cols="3">
+            <RightSidebar/>
+        </b-col>
+      </b-row>
+    </div>
+</template>
+<script>
+import RightSidebar from '../components/RightSidebar.vue'
+import { BRow, BCol } from 'bootstrap-vue'
+export default {
+  components: {
+    RightSidebar,
+    BRow,
+    BCol
+  }
+}
+</script>
+<style lang="">
+
+</style>
